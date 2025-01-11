3fd47211d948a8335f54f2c8885afd01a9024e30
Adam <adam1.kontakt@gmail.com>
2025-01-11 22:32:01 +0100

    dodanie funkcji pomocniczej do wypisywania pozycji statku podczas ustawiania (do debugowania). dodanie funkcji do uruchamiania ustawiania statkow.

--- functions.h
+++ functions.h
@@ -8,6 +8,8 @@ void mouse_drag(int key, struct ship* s, Color color);//ustala położenie obiek
 void rotate(int key, Image* sprite, Texture2D* texture);   //ustala rotację obiektu
 void UpdateShip(bool* isDragging, struct ship* s);	//wywołuje mouse_drag i rotate
 void SnapToGrid(struct ship* s, int gridStartX, int gridStartY, int cellSize); //przypisuje statek do kratki
+void PrintShipPositions(struct ship*);//funkcja pomocnicza do wypisywania koordynatóœ statku
+GameData GameSet(); //uruchamia ustawianie statków
 /*plansza i interakcja z plansza*/
 board* initboard();
 void delboard(board* boardtab);
@@ -16,7 +18,7 @@ void shoot(board* player,pair pairA);//wymaga poprawnie zainicjowanego statku ze
 void printboard(board* boardtab);//funkcja rysuje tablice danego gracza. Na razie drukuje tylko tile'a (funkcja potencjalnie do wymiany)
 struct array_cordinals* Get_array_cordinals(int offsetX, int offsetY);//przekształca położenie myszki na położenie statku na planszy
 /*gameplay*/
-void PlayGame(board *playerBoard, board *enemyBoard, ship *playerShip, ship *enemyShip); //funkcja do obslugi gry (inicjuje okienko oraz dodany jest jakis biedny postgame)
+void PlayGame(board *playerBoard, board *enemyBoard, ship *playerShips, ship *enemyShip); //funkcja do obslugi gry (inicjuje okienko oraz dodany jest jakis biedny postgame)
 /*do dodania*/
 ship* initship(int type);
 void delship(ship* statek);
