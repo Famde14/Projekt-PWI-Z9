ae864d13d65bdc26de109ff08afab4fb42070381
Bartłomiej Powierża <mozartwa123@gmail.com>
2025-01-18 11:45:35 +0100

    Uwaga będzie revert

--- functions.c
+++ functions.c
@@ -24,7 +24,7 @@ void mouse_drag(int key, ship* s, Color color){	//ustala położenie obiektu po 
 }
 
 void rotate(int key, Image* sprite, Texture2D* texture){	//ustala rotację obiektu
-	UnloadTexture(*texture);					//usuwa poprednią teksturę
+	//UnloadTexture(*texture);					//usuwa poprednią teksturę
 
 	if(key=='E') ImageRotateCW(sprite);		//obrót zgodnie z ruchem wskazówek zegara
 	else ImageRotateCCW(sprite);				//obrót przeciwnie z ruchem wskazówek zegara
@@ -534,7 +534,8 @@ GameData GameSet()
 	printf("Contents of playerBoard:\n");
     printboard(playerBoard);
 
-    // Unload textures
+    // Unload textures - ponoć CloseWindow() zachowuje się jak Garbage Collector
+    /*
     for (int i = 0; i < 4; i++) 
     {
         UnloadTexture(ship1Textures[i]);
@@ -550,7 +551,7 @@ GameData GameSet()
     for (int i = 0; i < 1; i++) 
     {
         UnloadTexture(ship4Textures[i]);
-    }
+    }*/
 
     GameData gameData = {playerBoard, playerShips, MAX_SHIPS};
     return gameData;
@@ -697,7 +698,7 @@ void placeStatek(board *boardtab, ship *curr_ship, pair begin, int direction) //
 		}
 		for (int i = 0; i < (int)(curr_ship->type); i++)
 		{
-			shiptile temp = {begin.x, (begin.y + i)};
+			shiptile temp = {{begin.x, (begin.y + i)}, 0};
 			boardtab->BOARD[(unsigned int)begin.x][(unsigned int)begin.y + i] = curr_ship;
 			curr_ship->boardplace[i] = temp;
 		}
@@ -719,7 +720,7 @@ void placeStatek(board *boardtab, ship *curr_ship, pair begin, int direction) //
 		}
 		for (int i = 0; i < (int)(curr_ship->type); i++)
 		{
-			shiptile temp = {begin.x + i, (begin.y)};
+			shiptile temp = {{begin.x + i, (begin.y)}, 0};
 			boardtab->BOARD[(unsigned int)begin.x + i][(unsigned int)begin.y] = curr_ship;
 			curr_ship->boardplace[i] = temp;
 		}
@@ -863,8 +864,8 @@ void ResetGame(board **playerBoard, board **enemyBoard, ship **playerShip, ship 
 {
     delboard(*playerBoard);
     delboard(*enemyBoard);
-    delship(*playerShip);
-    delship(*enemyShip);
+    //delship(*playerShip);
+    //delship(*enemyShip);
 
     *playerBoard = initboard();
     *enemyBoard = initboard();
